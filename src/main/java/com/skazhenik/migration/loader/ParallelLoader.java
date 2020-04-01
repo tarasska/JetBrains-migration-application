a022fa41afefeaafb7668f7390d336fef2cd35b2
tarasska <skazhenik.taras@niuitmo.ru>
2020-04-01 11:42:33 +0300

    add MigrationManager methods

---
+++ src/main/java/com/skazhenik/migration/loader/ParallelLoader.java
@@ -0,0 +1,4 @@
+package com.skazhenik.migration.loader;
+
+public class ParallelLoader {
+}
--- src/main/java/com/skazhenik/migration/util/MigrationManager.java
+++ src/main/java/com/skazhenik/migration/util/MigrationManager.java
@@ -4,6 +4,8 @@ import com.skazhenik.migration.exception.MigrationException;
 import com.skazhenik.migration.exception.ServiceException;
 import com.skazhenik.migration.service.AbstractStorageService;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.List;
 
 public class MigrationManager {
@@ -20,5 +22,35 @@ public class MigrationManager {
             }
         }
         throw new MigrationException("Waiting too long for the correct response to the file list request");
+    }
+
+    public static void downloadFile(final AbstractStorageService service,
+                                    final Path tempDir,
+                                    final String fileName) throws MigrationException {
+        int remainingAttempts = UnsuccessfulRequestCount;
+        while (remainingAttempts > 0) {
+            try {
+                service.download(tempDir, fileName);
+            } catch (ServiceException e) {
+                System.err.println("Log `download` (file: " + fileName + ") failed: " + e.getMessage());
+                remainingAttempts--;
+            }
+        }
+        throw new MigrationException("Waiting too long for the correct response to the file download request");
+    }
+
+    public static void uploadFile(final AbstractStorageService service,
+                                  final File file,
+                                  final String fileName) throws MigrationException {
+        int remainingAttempts = UnsuccessfulRequestCount;
+        while (remainingAttempts > 0) {
+            try {
+                service.upload(file, fileName);
+            } catch (ServiceException e) {
+                System.err.println("Log `upload` (file: " + fileName + ") failed: " + e.getMessage());
+                remainingAttempts--;
+            }
+        }
+        throw new MigrationException("Waiting too long for the correct response to the file upload request");
     }
 }
