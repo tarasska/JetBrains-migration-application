449dff741ab57db140ed3387b92596214608ebce
tarasska <skazhenik.taras@niuitmo.ru>
2020-04-03 16:03:46 +0300

    add tests

--- src/test/java/com/skazhenik/migration/StorageServiceTest.java
+++ src/test/java/com/skazhenik/migration/StorageServiceTest.java
@@ -2,6 +2,7 @@ package com.skazhenik.migration;
 
 import com.skazhenik.migration.exception.MigrationException;
 import com.skazhenik.migration.service.NewStorageService;
+import com.skazhenik.migration.service.OldStorageService;
 import com.skazhenik.migration.util.MigrationUtils;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
@@ -49,18 +50,18 @@ public class StorageServiceTest {
 
         NewStorageService newStorageService = new NewStorageService();
         for (int i = 1; i < SMALL_TEST_SIZE; i++) {
-            final String name = "file" + i + ".txt";
+            final String name = "loadNewStorageTestFile" + i + ".txt";
             final Path file = tempDir1.resolve(name);
             generateRandomFile(file, i);
             try {
                 MigrationUtils.uploadFile(newStorageService, new File(file.toString()), name);
             } catch (MigrationException e) {
-                Assert.fail("Uploading failed");
+                Assert.fail("Uploading failed " + e.getMessage());
             }
             try {
                 MigrationUtils.downloadFile(newStorageService, tempDir2, name);
             } catch (MigrationException e) {
-                Assert.fail("Downloading failed");
+                Assert.fail("Downloading failed " + e.getMessage());
             }
             try {
                 Assert.assertEquals(
@@ -68,20 +69,20 @@ public class StorageServiceTest {
                         FileUtils.readFileToString(new File(tempDir2.resolve(name).toString()), StandardCharsets.UTF_8)
                 );
             } catch (IOException e) {
-                Assert.fail("Files comparing failed");
+                Assert.fail("Files comparing failed " + e.getMessage());
             }
         }
     }
 
     @Test
-    public void getFilesNewStorageTest() {
+    public void getFilesUploadNewStorageTest() {
         Path tempDir = createDir();
         Objects.requireNonNull(tempDir);
 
         Set<String> names = new HashSet<>();
         NewStorageService newStorageService = new NewStorageService();
         for (int i = 1; i < SMALL_TEST_SIZE; i++) {
-            final String name = "file" + i + ".txt";
+            final String name = "getFilesUploadNewStorageTestFile" + i + ".txt";
             final Path file = tempDir.resolve(name);
             generateRandomFile(file, i);
             names.add(name);
@@ -94,7 +95,35 @@ public class StorageServiceTest {
         try {
             Assert.assertEquals(names, new HashSet<>(MigrationUtils.getFilesList(newStorageService)));
         } catch (MigrationException e) {
-            Assert.fail("getFilesList failed");
+            Assert.fail("getFilesList failed " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void getFilesDeleteOldStorageTest() {
+        OldStorageService oldStorageService = new OldStorageService();
+        Set<String> oldStorageNames = new HashSet<>();
+        try {
+            oldStorageNames.addAll(MigrationUtils.getFilesList(oldStorageService));
+        } catch (MigrationException e) {
+            Assert.fail("getFilesList from old storage failed " + e.getMessage());
+        }
+        int i = 0;
+        for (Iterator<String> it = oldStorageNames.iterator(); it.hasNext(); ) {
+            try {
+                MigrationUtils.deleteFile(oldStorageService, it.next());
+                it.remove();
+            } catch (MigrationException ignored) {
+            }
+            if (i++ > SMALL_TEST_SIZE) {
+                break;
+            }
+        }
+
+        try {
+            Assert.assertEquals(oldStorageNames, new HashSet<>(MigrationUtils.getFilesList(oldStorageService)));
+        } catch (MigrationException e) {
+            Assert.fail("getFilesList failed " + e.getMessage());
         }
     }
 }
